bc79f512dc557ab02ac64314bc52982927953105
kit.perez <kit.perez@gmail.com>
2017-07-24 22:37:04 +1000

    added raw views and initial routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+
+  def home
+    render :home
+  end
+
+  def show_login
+    render :login
+  end
+
+  def login
+    redirect_to '/deals'
+  end
+
+  def show_signup
+    render :signup
+  end
+
+  def signup
+    redirect_to '/deals'
+  end
+
+  def deals
+    render :dashboard
+  end
+
+  def show_save_deal
+    render :savedeal
+  end
+
+  def add_to_favorites
+    redirect_to '/deals'
+  end
+end
