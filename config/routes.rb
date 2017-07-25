2e4a4cf3f2bcbe9eeb975945841b6d704e9c1738
Sam Young <samwilsonyoung@gmail.com>
2017-07-25 15:31:26 +1000

    work on signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :deals
   resources :users
 
-  enable sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'pages#home'
